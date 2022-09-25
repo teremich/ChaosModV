cda2a49f6a59e9d4261a5add25b3c2e8d928b789
= <=>
2022-09-25 11:57:54 +0200

    Reworked and moved the TwitchVotingReceiver

---
+++ TwitchChatVotingProxy/VotingReceiver/TwitchVotingReceiver.cs
@@ -0,0 +1,104 @@
+﻿using Serilog;
+using Shared;
+using System;
+using System.Threading.Tasks;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Events;
+
+namespace TwitchChatVotingProxy.VotingReceiver
+{
+    internal class TwitchVotingReceiver : IDisposable, IVotingReceiver
+    {
+        public event EventHandler<OnMessageArgs>? OnMessage;
+
+        private static readonly int RECONNECT_INTERVAL = 1000;
+
+        private ILogger logger = Log.Logger.ForContext<TwitchVotingReceiver>();
+        private TwitchClient twitchClient;
+
+        public TwitchVotingReceiver(OptionsFile options)
+        {
+            var userName = options.RequireString("TwitchUserName");
+            logger.Information($"specified username: '{userName}'");
+
+            var channelName = options.RequireString("TwitchChannelName");
+            logger.Information($"trying to connect to cannel '{channelName}'...");
+
+            var oauth = options.RequireString("TwitchChannelOAuth");
+
+            twitchClient = new TwitchClient(new WebSocketClient());
+            twitchClient.Initialize(
+                new ConnectionCredentials(userName, oauth),
+                channelName
+            );
+            twitchClient.OnConnected += OnConnected;
+            twitchClient.OnError += OnError;
+            twitchClient.OnIncorrectLogin += OnIncorrectLogin;
+            twitchClient.OnJoinedChannel += OnJoinedChannel;
+            twitchClient.OnMessageReceived += OnMessageReceived;
+            twitchClient.Connect();
+        }
+
+        public void Dispose()
+        {
+            twitchClient.Disconnect();
+        }
+
+        public void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnConnected(object? sender, OnConnectedArgs e)
+        {
+            logger.Information("successfully connected to twitch!");
+        }
+
+        private async void OnDisconnect(object sender, OnDisconnectedArgs e)
+        {
+            logger.Error(
+                "disconnected from twitch channel... trying to reconnect..."
+            );
+
+            await Task.Delay(RECONNECT_INTERVAL);
+
+            twitchClient.Connect();
+        }
+
+        private void OnError(object? sender, OnErrorEventArgs e)
+        {
+            logger.Error(
+                "experienced an exception, disconnecting...",
+                e.Exception
+            );
+            twitchClient.Disconnect();
+        }
+
+        private void OnIncorrectLogin(object? sender, OnIncorrectLoginArgs e)
+        {
+            logger.Error("incorrect twitch login, verify user name and oauth");
+            twitchClient.Disconnect();
+        }
+
+        private void OnJoinedChannel(object? sender, OnJoinedChannelArgs e)
+        {
+            logger.Information($"successfully joined twitch channel '{e.Channel}'");
+        }
+
+        private void OnMessageReceived(object? sender, OnMessageReceivedArgs e)
+        {
+            var chatMessage = e.ChatMessage;
+
+            var args = new OnMessageArgs(
+                chatMessage.UserId,
+                chatMessage.Message.Trim(),
+                chatMessage.Username.ToLower()
+            );
+
+            OnMessage?.Invoke(this, args);
+        }
+    }
+}
